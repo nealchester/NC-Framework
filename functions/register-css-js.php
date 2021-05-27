38ec9da4b80db52927a6f1e7edb122a8f9a55328
nealchester <ncchester@gmail.com>
2021-05-26 21:19:51 -0400

    commit

--- functions/register-css-js.php
+++ functions/register-css-js.php
@@ -7,18 +7,16 @@ function nc_register_assets(){
   // JS
 
   wp_register_script('standard-scripts', get_theme_file_uri('/js/standard-scripts.js'), array('jquery'), '1', true);
-
-  wp_register_script('sal-aniview', get_theme_file_uri('/js/aniview/aniview.js'), array( 'jquery' ), null, true);
-  wp_register_script('sal-aniview-init', get_theme_file_uri('/js/aniview/aniview-init.js'), array( 'sal-aniview' ), null, true);
+  wp_register_script('sal-aniview', get_theme_file_uri('/js/aniview/aniview.js'), array( 'jquery' ), null, false);
+  wp_register_script('sal-aniview-init', get_theme_file_uri('/js/aniview/aniview-init.js'), array( 'sal-aniview' ), null, false);
 
   // CSS
 
   wp_register_style('animate-css', get_theme_file_uri('/js/aniview/animate.css'), null, '3.7.2', 'screen');
-
   wp_register_style('nc-editor', get_theme_file_uri('/css/editor.css'), array('nc-vars'), null, 'screen');
   wp_register_style('nc-vars', get_theme_file_uri('/css/variables.css'), null, null, 'screen');
+  wp_register_style('nc-uclasses', get_theme_file_uri('/css/uclasses.css'), array('nc-vars'), null, 'screen');
   wp_register_style('nc-root', get_theme_file_uri('/css/root.css'), array('nc-vars'), null, 'screen');
-  wp_register_style('nc-uclasses', get_theme_file_uri('/css/uclasses.css'), array('nc-root'), null, 'screen');
   wp_register_style('nc-blocks', get_theme_file_uri('/css/blocks.css'), array('nc-root'), null, 'screen');
   wp_register_style('nc-menus', get_theme_file_uri('/css/menus.css'), array('nc-root'), null, 'screen');
   wp_register_style('nc-content', get_theme_file_uri('/css/content.css'), array('nc-blocks'), null, 'screen');
@@ -33,20 +31,22 @@ add_action('wp_enqueue_scripts', 'nc_register_assets');
 function nc_load_assets(){
   if( !is_admin() ){
     
-    // CSS
+    // CSS    
+    wp_enqueue_style('animate-css'); 
     wp_enqueue_style('nc-vars');
-    wp_enqueue_style('nc-blocks');
-    wp_enqueue_style('nc-content');
     wp_enqueue_style('nc-root');
-    wp_enqueue_style('nc-theme');
-    wp_enqueue_style('nc-menus');
     wp_enqueue_style('nc-uclasses');
+    wp_enqueue_style('nc-blocks');
+    wp_enqueue_style('nc-menus');
+    wp_enqueue_style('nc-content');
+    wp_enqueue_style('nc-theme');
 
     wp_enqueue_style('wp-mediaelement', false);
     wp_enqueue_style('mediaelement', false);
-    wp_enqueue_style('animate-css'); 
+
 
     // JS
+    wp_enqueue_script('standard-scripts');
     wp_enqueue_script('sal-aniview');
     wp_enqueue_script('sal-aniview-init');
   }
--- js/standard-scripts.js
+++ js/standard-scripts.js
@@ -55,14 +55,13 @@ jQuery(window).scroll(function() {
 		// First Word selection
 		// https://www.jaredatchison.com/code/use-jquery-manipulate-first-last-word-element/
 
-		jQuery(function($){
-		// First word in title
-		$('.nc-first-word').html(function(){	
-		var text = $(this).text().split(' ');
-		var first = text.shift();
-		return (text.length > 0 ? '<span class="fw">'+first+'</span> ' : first) + text.join(" ");
+
+		jQuery('.nc-first-word').html(function(){	
+			var text = jQuery(this).text().split(' ');
+			var first = text.shift();
+			return (text.length > 0 ? '<span class="fw">'+first+'</span> ' : first) + text.join(" ");
 		});
-		});		
+	
 
 
 		// Automatic wrap images with class name 'imagewrap' to apply inline styles.
