7ae865c9cecdb799665754c048c1760a63861726
nealchester <ncchester@gmail.com>
2021-07-08 16:24:27 -0400

    body class update

--- functions/body-classes.php
+++ functions/body-classes.php
@@ -6,6 +6,10 @@ function nc_add_body_classes($classes)
     if (get_theme_mod('enhance_radio_checkboxes', true) == true) {
         $classes[] = 'formstyles';
     }
+    if(is_singular()) {
+        $category = get_the_category();
+        $classes[] = 'category-'.strtolower($category[0]->slug);
+    } else { }
     return $classes;
 }
 
