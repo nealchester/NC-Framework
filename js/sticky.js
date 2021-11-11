dd8625d202823cdfd135757c7c783a456b0e0535
nealchester <ncchester@gmail.com>
2021-11-10 22:13:39 -0500

    sticky script updated

--- js/sticky.js
+++ js/sticky.js
@@ -1,14 +1,12 @@
-/*
-Sticky header.
-https://css-tricks.com/how-to-detect-when-a-sticky-element-gets-pinned/
-Add the class "ncsticky" to any element that is using "position: sticky; top-1px" 
-and a new class of "ncsticky-enabled" will be added once the element is stuck. 
+//http://talkerscode.com/webtricks/create-sticky-header-using-jquery.php
 
-NOTE: This will only work on one element. To support more elements, duplicate this code and create new classes.
-*/
-
-const el = document.querySelector(".ncsticky")
-const observer = new IntersectionObserver( 
-([e]) => e.target.classList.toggle("ncsticky-enabled", e.intersectionRatio < 1),
-{ threshold: [1] }
-); observer.observe(el);
+jQuery(window).scroll(function() {
+  if (jQuery(this).scrollTop() > 1)
+  {
+   jQuery('#header').addClass("sticky-enabled");
+  }
+  else
+  {
+   jQuery('#header').removeClass("sticky-enabled");
+  }
+ });
