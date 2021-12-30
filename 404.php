147db6faf9e9ff6873ffbe13890e4d0864fe689b
nealchester <ncchester@gmail.com>
2021-12-29 22:28:27 -0500

    commit

--- 404.php
+++ 404.php
@@ -1,6 +1,6 @@
-<?php get_template_part('parts/layout_begin');?>
+<?php get_template_part('parts/header');?>
 <?php get_template_part('parts/the-title');?>
 <p><?php _e('Bummer, that page doesn&#8217t exist. It might&#8217ve moved or it was deleted. Do a search below:','nc-framework');?></p>
 <?php get_template_part('parts/searchform'); ?>
 <div style="height:50vh;"></div>
-<?php get_template_part('parts/layout_end');?>
+<?php get_template_part('parts/footer');?>
