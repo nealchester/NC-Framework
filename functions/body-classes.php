8e36acc32f0fc2ed97bf1afdd752053c8193a54e
nealchester <ncchester@gmail.com>
2022-04-15 18:35:25 -0400

    commit

--- functions/body-classes.php
+++ functions/body-classes.php
@@ -4,7 +4,7 @@ function nc_add_body_classes($classes){
     if (get_theme_mod('enhance_radio_checkboxes', true) == true) {
         $classes[] = 'formstyles';
     }
-    if(is_singular()) {
+    if( is_singular() && !is_home() && !is_front_page() ) {
         $category = get_the_category(); 
         $newcat = 'category-'.strtolower(@$category[0]->slug);
         $classes[] = $newcat;
