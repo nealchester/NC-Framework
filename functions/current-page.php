49555b5ff5d9cce660ffd5cd9762ab69761887d4
Neal Chester <ncchester@gmail.com>
2024-09-16 10:39:44 -0400

    Added classes to output

--- functions/current-page.php
+++ functions/current-page.php
@@ -21,5 +21,5 @@ function nc_current_page( $var = '' ) {
     if( $pages < 1 )
         return;
     $page = get_query_var( 'paged' ) ? get_query_var( 'paged' ) : 1;
-    echo '<p class="ncurrentpage txt-small">'.__('You\'re on page', 'nc-framework').' '. $page . ' '.__('of','nc-framework').' ' . $pages.'</p>';
+    echo '<p class="ncurrentpage">'.__('<span class="ncurrentpage_label">You\'re on page</span>', 'ul-countries').' <span class="ncurrentpage_number">'.$page. '</span> <span class="ncurrentpage_of">'.__('of','ul-countries').'</span> <span class="ncurrentpage_total">'.$pages.'</span></p>';
 }
