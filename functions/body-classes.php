c006f3a7df9e146d7dbb74b11dbe5352bd5f3000
Neal Chester <ncchester@gmail.com>
2025-03-27 22:42:09 -0400

    body class update

--- functions/body-classes.php
+++ functions/body-classes.php
@@ -1,10 +1,17 @@
 <?php
 
 function nc_add_body_classes($classes){
+
+    /* Add a class of site ID to to style site with in multisite. */
+    $this_id = get_current_blog_id();
+    $classes[] = 'site-id-'.$this_id;
+    
+    /* Add this class to style radio buttons and checkboxes in a different way. */
     if (get_theme_mod('enhance_radio_checkboxes', true) == true) {
         $classes[] = 'formstyles';
     }
 
+    /* Add this class to style posts based on category name. */
     if ( is_singular() ) {
         $category = get_the_category(); 
         if ( ! empty( $category ) ) {
